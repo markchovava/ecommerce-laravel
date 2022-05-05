9f9bae64a13c4d6e6b856cc051c2374139de899c
Mark Chovava <markchovava@gmail.com>
2022-05-05 21:20:48 +0200

    Designing layout almost done

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,97 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('frontend.layouts.main');
+
+Route::get('/signin', function () {
+    return view('auth.login1');
 });
+
+Route::get('/signup', function () {
+    return view('auth.register1');
+});
+
+
+/* 
+::::::::::::::::::::::::::::::::::::::
+        START OF FRONTEND
+::::::::::::::::::::::::::::::::::::::
+*/
+Route::get('/', function () {
+    return view('frontend.pages.index');
+});
+
+Route::get('/single', function () {
+    return view('frontend.pages.single');
+});
+
+Route::get('/shop', function () {
+    return view('frontend.pages.shop');
+});
+
+Route::get('/list', function () {
+    return view('frontend.pages.list');
+});
+
+Route::get('/search', function () {
+    return view('frontend.pages.search_results');
+});
+
+Route::get('/cart', function () {
+    return view('frontend.pages.cart');
+});
+
+Route::get('/checkout', function () {
+    return view('frontend.pages.checkout');
+});
+
+Route::get('/contact', function () {
+    return view('frontend.pages.contact');
+});
+
+Route::get('/about', function () {
+    return view('frontend.pages.about');
+});
+
+Route::get('/terms', function () {
+    return view('frontend.pages.terms');
+});
+
+
+/* 
+::::::::::::::::::::::::::::::::::::::
+        END OF FRONTEND
+::::::::::::::::::::::::::::::::::::::
+*/
+
+/* 
+::::::::::::::::::::::::::::::::::::::
+        START OF FRONTEND
+::::::::::::::::::::::::::::::::::::::
+*/
+Route::prefix('/admin')->group(function(){
+    Route::get('/', function () {
+        return view('backend.index');
+    });
+    Route::get('/products', function () {
+        return view('backend.products.index');
+    });
+    Route::get('/products/add', function () {
+        return view('backend.products.add');
+    });
+    Route::get('/products/edit', function () {
+        return view('backend.products.edit');
+    });
+    
+});
+
+/* 
+::::::::::::::::::::::::::::::::::::::
+        END OF FRONTEND
+::::::::::::::::::::::::::::::::::::::
+*/
+
+
+
 
 Route::middleware([
     'auth:sanctum',
